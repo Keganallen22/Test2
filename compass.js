0b640d66a32aaf16aef5fc8dcdb7cb2e64c1832a
Keganallen22 <65839974+Keganallen22@users.noreply.github.com>
2023-07-24 19:41:21 -0500

    Add files via upload

--- compass.js
+++ compass.js
@@ -1,48 +1,35 @@
-// if (window.DeviceOrientationEvent) {
-//   // Listen for the deviceorientation event and handle the raw data
-//   window.addEventListener('deviceorientation', function(eventData) {
-//     var compassdir;
-//
-//     if(event.webkitCompassHeading) {
-//       // Apple works only with this, alpha doesn't work
-//       compassdir = event.webkitCompassHeading;
-//     }
-//     else compassdir = event.alpha;
-//   });
-//   console.log(compassdir);
-//   document.getElementById("Direction").innerText = compassdir;
-// }
+const startBtn = document.querySelector(".start-btn");
+const myPoint = document.querySelector(".my-point");
+let compass;
+const isIOS = !(
+  navigator.userAgent.match(/(iPod|iPhone|iPad)/) &&
+  navigator.userAgent.match(/AppleWebKit/)
+);
 
 function init() {
-        var compass = document.getElementById('compass');
-        if(window.DeviceOrientationEvent) {
+  startBtn.addEventListener("click", startCompass);
+}
 
-          window.addEventListener('deviceorientation', function(event) {
-                var alpha;
-                //Check for iOS property
-                if(event.webkitCompassHeading) {
-                  alpha = event.webkitCompassHeading;
-                  //Rotation is reversed for iOS
-                  // compass.style.WebkitTransform = 'rotate(-' + alpha + 'deg)';
-                     // document.getElementById("Compass").innerText = alpha;
-
-                }
-                //non iOS
-                else {
-                  alpha = event.alpha;
-                  webkitAlpha = alpha;
-                  if(!window.chrome) {
-                    //Assume Android stock (this is crude, but good enough for our example) and apply offset
-                    webkitAlpha = alpha-270;
-                  }
-                }
-                document.getElementById("Compass").innerText = alpha;
-                document.getElementById("Compass2").innerText = webkitAlpha;
-
-                // compass.style.Transform = 'rotate(' + alpha + 'deg)';
-                // compass.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
-                //Rotation is reversed for FF
-                // compass.style.MozTransform = 'rotate(-' + alpha + 'deg)';
-              }, false);
+function startCompass() {
+  if (isIOS) {
+    DeviceOrientationEvent.requestPermission()
+      .then((response) => {
+        if (response === "granted") {
+          window.addEventListener("deviceorientation", handler, true);
+        } else {
+          alert("has to be allowed!");
         }
-      }
+      })
+      .catch(() => alert("not supported"));
+  } else {
+    window.addEventListener("deviceorientationabsolute", handler, true);
+  }
+}
+
+function handler(e) {
+  compass = e.webkitCompassHeading || Math.abs(e.alpha - 360);
+  // compassCircle.style.transform = `translate(-50%, -50%) rotate(${-compass}deg)`;
+  document.getElementById("compass").innerText = compass;
+}
+
+init();
